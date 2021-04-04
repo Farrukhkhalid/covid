f3e0f7cc9bfc7b204a829b10ffc7e7fccde5c772
Farrukhkhalid <farrukhkhalid.it@outlook.com>
2021-04-04 14:42:10 -0700

    new commit

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -7,22 +7,45 @@ const StyleRecovered = {
     background: '#27ae60',
     color: 'white',
     textAlign: 'left',
-    margin: '0 1% 1%'
+    margin: '1% 1% 1%'
 } ;
 
 const Styleinfected = {
     background: '#3498db',
     color: 'white',
     textAlign: 'left',
-    margin: '0 1% 1%'
+    margin: '1% 1% 1%'
 };
 const StyleDeaths ={
     background: '#e74c3c',
     color: 'white',
     textAlign: 'left',
-    margin: '0 1% 1%'
+    margin: '1% 1% 1%'
 } ;
 
+
+
+const StyleDailyRecov = {
+
+    borderBottom : '5px solid #27ae60' ,
+    color: '#2b2b2b',
+    textAlign: 'left',
+    margin: '1% 1% 1%' ,
+    
+} ;
+
+const Style2 = {
+    background: '#3498db',
+    color: 'white',
+    textAlign: 'left',
+    margin: '1% 1% 1%'
+};
+const Style3 ={
+    background: '#e74c3c',
+    color: 'white',
+    textAlign: 'left',
+    margin: '0% 1% 1%'
+} ;
 
 
 
@@ -34,8 +57,8 @@ function Cards(props) {
       <Grid container spacing={3} justify="center">
          <Grid item component={Card} style={Styleinfected}>
           <CardContent className='Card' >
-            <Typography>INFECTED</Typography>
-            <Typography variant="h5">
+            <Typography>CONFIRMED</Typography>
+            <Typography variant="h4">
             <Countup
                 start={0}
                 end={props.data.recovered}
@@ -53,7 +76,7 @@ function Cards(props) {
         <Grid item component={Card} style={StyleRecovered}>
         <CardContent className='Card'>
             <Typography >RECOVERED</Typography>
-            <Typography variant="h5">
+            <Typography variant="h4">
                 <Countup
                 start={0}
                 end={props.data.deaths}
@@ -69,6 +92,79 @@ function Cards(props) {
         <Grid item component={Card} style={StyleDeaths}>
         <CardContent className='Card'>
             <Typography color="textSecoundary">DEATHS</Typography>
+            <Typography variant="h4"><Countup
+                start={0}
+                end={props.data.confirmed}
+                duration={2}
+                separator=","
+              /></Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              number of active cases in covid
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+
+
+
+
+
+
+      <Grid container spacing={0} justify="center">
+         <Grid item component={Card} style={StyleDailyRecov}>
+          <CardContent className='Card' >
+            <Typography>INFECTED</Typography>
+            <Typography variant="h5">
+            <Countup
+                start={0}
+                end={props.data.recovered}
+                duration={1}
+                separator=","
+              />
+            </Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              number of active cases in covid
+            </Typography>
+          </CardContent>
+       </Grid>
+       <Grid item component={Card} style={StyleDailyRecov}>
+          <CardContent className='Card' >
+            <Typography>INFECTED</Typography>
+            <Typography variant="h5">
+            <Countup
+                start={0}
+                end={props.data.recovered}
+                duration={1}
+                separator=","
+              />
+            </Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              number of active cases in covid
+            </Typography>
+          </CardContent>
+       </Grid>
+        <Grid item component={Card} style={StyleDailyRecov}>
+        <CardContent className='Card'>
+            <Typography >RECOVERED</Typography>
+            <Typography variant="h5">
+                <Countup
+                start={0}
+                end={props.data.deaths}
+                duration={2}
+                separator=","
+              /></Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">
+              number of active cases in covid
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} style={StyleDailyRecov}>
+        <CardContent className='Card'>
+            <Typography color="textSecoundary">DEATHS</Typography>
             <Typography variant="h5"><Countup
                 start={0}
                 end={props.data.confirmed}
@@ -82,6 +178,24 @@ function Cards(props) {
           </CardContent>
         </Grid>
       </Grid>
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
